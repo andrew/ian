7f6e0c5d14a2dcb10cd671f1ebc67b1131cac2cc
Andrew Nesbitt <andrewnez@gmail.com>
2009-04-11 22:25:42 +0100

    Added alert sound (Submarine from osx) every 5 mins, HTML5 Audio used, only works in Safari

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,10 @@
 $.timer(300000, function (timer) {
-  // ajax get a the twitter update
+  // ajax get a the twitter update ever 5 minutes
+  playAlert();
 });
+
+function playAlert(){
+  // this only works in safari
+  var audio = new Audio("../audio/Submarine.aiff");
+  audio.play();
+}
