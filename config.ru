1ed9c9af95202d9de5637072469fb47acbcbe2b9
Andrew Nesbitt <andrewnez@gmail.com>
2011-04-08 11:10:17 +0100

    Make it work on heroku

---
+++ config.ru
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+require './ian'
+
+run Sinatra::Application
--- ian.rb
+++ ian.rb
@@ -1,7 +1,3 @@
-require 'rubygems'
-require 'bundler'
-Bundler.require
-
 set :haml, {:format => :html5 }
 
 get '/' do
